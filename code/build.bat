695f24162e526aae43f321e17b77e63da781a18c
Ashley Morton-Bye <junglie85@gmail.com>
2023-02-21 21:18:37 +0000

    Draw a bitmap and clear the screen

--- code/build.bat
+++ code/build.bat
@@ -3,6 +3,28 @@
 IF NOT EXIST %~dp0\..\..\build mkdir %~dp0\..\..\build
 pushd %~dp0\..\..\build
 
-cl /nologo /std:c17 /FC /Zi /Wall /WX /external:anglebrackets /external:W0 ..\breakout\code\win32_breakout.c /link /WX user32.lib gdi32.lib
+cl /nologo /std:c17 /FC /Zi /Wall /WX /wd4820 /external:anglebrackets /external:W0 ..\breakout\code\win32_breakout.c /link /WX user32.lib gdi32.lib /incremental:no /opt:ref
 
 popd
+
+REM Compiler flags:
+
+REM /nologo  : don't show the logo
+REM /std:c17 : use the c17 standard
+REM /FC      : use full path to source file in messages
+REM /Zi      : generate debug info
+REM /Wall    : enable all warnings
+REM /WX      : treat warnings as errors
+REM /wdXXXX  : disable warning XXXX
+REM /external:anglebrackets : treat includes in angle brackets as third party source
+REM /external:W0            : disable all warnings in third party code
+
+REM Linker flags:
+
+REM /WX            : treat warnings as errors
+REM incremental:no : disable incremental builds
+REM opt:ref        : exclude unused functions and data
+
+REM Compiler warnings:
+
+REM C4820 : 'bytes' bytes padding added after construct 'member_name'
--- code/win32_breakout.c
+++ code/win32_breakout.c
@@ -2,10 +2,19 @@
 #include <windows.h>
 
 #include <stdbool.h>
+#include <stdint.h>
 
 #define UNUSED(X) ((void)(X))
 
-static bool running;
+struct win32_render_buffer {
+    uint32_t width;
+    uint32_t height;
+    uint32_t *memory;
+    BITMAPINFO info;
+};
+
+static bool global_running;
+static struct win32_render_buffer global_render_buffer;
 
 static LRESULT win32_window_procedure(HWND window, UINT message, WPARAM w_param, LPARAM l_param)
 {
@@ -13,15 +22,51 @@ static LRESULT win32_window_procedure(HWND window, UINT message, WPARAM w_param,
 
     switch (message) {
     case WM_CLOSE:
-    case WM_DESTROY:
-        running = false;
+    case WM_DESTROY: {
+        global_running = false;
         break;
+    }
+
+    case WM_SIZE: {
+        RECT rect;
+        GetWindowRect(window, &rect);
+        global_render_buffer.width = rect.right - rect.left;
+        global_render_buffer.height = rect.bottom - rect.top;
+
+        if (global_render_buffer.memory) {
+            VirtualFree(global_render_buffer.memory, 0, MEM_RELEASE);
+        }
+
+        global_render_buffer.memory = VirtualAlloc(0,
+            sizeof(uint32_t) * global_render_buffer.width * global_render_buffer.height,
+            MEM_COMMIT | MEM_RESERVE,
+            PAGE_READWRITE);
+
+        global_render_buffer.info.bmiHeader.biSize = sizeof(global_render_buffer.info.bmiHeader);
+        global_render_buffer.info.bmiHeader.biWidth = global_render_buffer.width;
+        global_render_buffer.info.bmiHeader.biHeight = global_render_buffer.height;
+        global_render_buffer.info.bmiHeader.biPlanes = 1;
+        global_render_buffer.info.bmiHeader.biBitCount = 32;
+        global_render_buffer.info.bmiHeader.biCompression = BI_RGB;
+
+        break;
+    }
 
     default:
         result = DefWindowProcW(window, message, w_param, l_param);
     }
 
     return result;
+}
+
+static void clear_screen(struct win32_render_buffer *render_buffer, uint32_t color)
+{
+    uint32_t *pixel = render_buffer->memory;
+    for (uint32_t y = 0; y < render_buffer->height; ++y) {
+        for (uint32_t x = 0; x < render_buffer->width; ++x) {
+            *pixel++ = color;
+        }
+    }
 }
 
 int WinMain(HINSTANCE instance, HINSTANCE previous_instance, LPSTR cmdline, int showcmd)
@@ -59,13 +104,28 @@ int WinMain(HINSTANCE instance, HINSTANCE previous_instance, LPSTR cmdline, int 
         return -1;
     }
 
-    running = true;
-    while (running) {
+    HDC device_context = GetDC(window);
+
+    global_running = true;
+    while (global_running) {
+        // Input.
         MSG message;
         while (PeekMessageW(&message, window, 0, 0, PM_REMOVE)) {
             TranslateMessage(&message);
             DispatchMessageW(&message);
         }
+
+        // Update.
+        clear_screen(&global_render_buffer, 0x00ff4400);
+
+        // Render.
+        StretchDIBits(device_context,
+            0, 0, (int)global_render_buffer.width, (int)global_render_buffer.height,
+            0, 0, (int)global_render_buffer.width, (int)global_render_buffer.height,
+            global_render_buffer.memory,
+            &global_render_buffer.info,
+            DIB_RGB_COLORS,
+            SRCCOPY);
     }
 
     return 0;
